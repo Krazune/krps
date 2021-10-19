1fe5298ce5d22e3736c1ef821a51a727cd12c256
Krazune <krazune@gmail.com>
2021-10-19 17:38:05 +0100

    Create logout static method

--- src/main/java/krazune/krps/user/actions/LogoutUser.java
+++ src/main/java/krazune/krps/user/actions/LogoutUser.java
@@ -5,19 +5,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import krazune.krps.user.Authentication;
 
 public class LogoutUser extends HttpServlet
 {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException
 	{
-		HttpSession session = request.getSession(false);
-
-		if (session != null)
-		{
-			session.removeAttribute("sessionUser");
-		}
+		Authentication.logOut(request.getSession(false));
 
 		response.sendRedirect("/");
 	}
