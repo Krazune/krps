faeb41c0e63e0462096c8caf8d30829a459b471c
Krazune <krazune@gmail.com>
2021-10-22 23:09:31 +0100

    Extract methods

--- src/main/java/krazune/krps/statistics/pagecontrollers/StatisticsPageController.java
+++ src/main/java/krazune/krps/statistics/pagecontrollers/StatisticsPageController.java
@@ -23,42 +23,7 @@ public class StatisticsPageController extends HttpServlet
 	{
 		try
 		{
-			PropertiesLoader propertiesLoader = (PropertiesLoader)request.getAttribute("propertiesLoader");
-			String jdbcUrl = propertiesLoader.getJdbcUrl();
-			String jdbcUsername = propertiesLoader.getJdbcUser();
-			String jdbcPassword = propertiesLoader.getJdbcPassword();
-
-			ConnectionFactory connectionFactory = new ConnectionFactory(jdbcUrl, jdbcUsername, jdbcPassword);
-			GameDAO gameDao = new GameDAO(connectionFactory);
-
-			request.setAttribute("gameCount", gameDao.getGameCount());
-			request.setAttribute("winCount", gameDao.getGameResultCount(GameResult.WIN));
-			request.setAttribute("lossCount", gameDao.getGameResultCount(GameResult.LOSS));
-			request.setAttribute("drawCount", gameDao.getGameResultCount(GameResult.DRAW));
-
-			request.setAttribute("userChoiceRockCount", gameDao.getUserChoiceCount(GameChoice.ROCK));
-			request.setAttribute("userChoicePaperCount", gameDao.getUserChoiceCount(GameChoice.PAPER));
-			request.setAttribute("userChoiceScissorsCount", gameDao.getUserChoiceCount(GameChoice.SCISSORS));
-
-			HttpSession session = request.getSession(false);
-
-			if (session != null && session.getAttribute("sessionUser") != null)
-			{
-				User sessionUser = (User)session.getAttribute("sessionUser");
-
-				request.setAttribute("sessionUserGameCount", gameDao.getGameCountByUser(sessionUser));
-				request.setAttribute("sessionUserWinCount", gameDao.getGameResultCountByUser(sessionUser, GameResult.WIN));
-				request.setAttribute("sessionUserLossCount", gameDao.getGameResultCountByUser(sessionUser, GameResult.LOSS));
-				request.setAttribute("sessionUserDrawCount", gameDao.getGameResultCountByUser(sessionUser, GameResult.DRAW));
-
-				request.setAttribute("sessionUserRockCount", gameDao.getUserChoiceCountByUser(sessionUser, GameChoice.ROCK));
-				request.setAttribute("sessionUserPaperCount", gameDao.getUserChoiceCountByUser(sessionUser, GameChoice.PAPER));
-				request.setAttribute("sessionUserScissorsCount", gameDao.getUserChoiceCountByUser(sessionUser, GameChoice.SCISSORS));
-
-				List<Game> lastGames = gameDao.findByUser(sessionUser, 10);
-
-				request.setAttribute("lastGames", lastGames);
-			}
+			populateStatisticsAttributes(request);
 		}
 		catch (SQLException e)
 		{
@@ -66,5 +31,63 @@ public class StatisticsPageController extends HttpServlet
 		}
 
 		request.getRequestDispatcher("/WEB-INF/jsp/statistics.jsp").forward(request, response);
+	}
+
+	private void populateStatisticsAttributes(HttpServletRequest request) throws SQLException
+	{
+		populateGlobalStatisticsAttributes(request);
+
+		HttpSession session = request.getSession(false);
+
+		if (session != null && session.getAttribute("sessionUser") != null)
+		{
+			populateUserStatisticsAttributes(request, session);
+		}
+	}
+
+	private void populateGlobalStatisticsAttributes(HttpServletRequest request) throws SQLException
+	{
+		PropertiesLoader propertiesLoader = (PropertiesLoader)request.getAttribute("propertiesLoader");
+		String jdbcUrl = propertiesLoader.getJdbcUrl();
+		String jdbcUsername = propertiesLoader.getJdbcUser();
+		String jdbcPassword = propertiesLoader.getJdbcPassword();
+
+		ConnectionFactory connectionFactory = new ConnectionFactory(jdbcUrl, jdbcUsername, jdbcPassword);
+		GameDAO gameDao = new GameDAO(connectionFactory);
+
+		request.setAttribute("gameCount", gameDao.getGameCount());
+		request.setAttribute("winCount", gameDao.getGameResultCount(GameResult.WIN));
+		request.setAttribute("lossCount", gameDao.getGameResultCount(GameResult.LOSS));
+		request.setAttribute("drawCount", gameDao.getGameResultCount(GameResult.DRAW));
+
+		request.setAttribute("userChoiceRockCount", gameDao.getUserChoiceCount(GameChoice.ROCK));
+		request.setAttribute("userChoicePaperCount", gameDao.getUserChoiceCount(GameChoice.PAPER));
+		request.setAttribute("userChoiceScissorsCount", gameDao.getUserChoiceCount(GameChoice.SCISSORS));
+	}
+
+	private void populateUserStatisticsAttributes(HttpServletRequest request, HttpSession session) throws SQLException
+	{
+		PropertiesLoader propertiesLoader = (PropertiesLoader)request.getAttribute("propertiesLoader");
+		String jdbcUrl = propertiesLoader.getJdbcUrl();
+		String jdbcUsername = propertiesLoader.getJdbcUser();
+		String jdbcPassword = propertiesLoader.getJdbcPassword();
+
+		ConnectionFactory connectionFactory = new ConnectionFactory(jdbcUrl, jdbcUsername, jdbcPassword);
+		GameDAO gameDao = new GameDAO(connectionFactory);
+
+		User sessionUser = (User)session.getAttribute("sessionUser");
+
+		request.setAttribute("sessionUserGameCount", gameDao.getGameCountByUser(sessionUser));
+		request.setAttribute("sessionUserWinCount", gameDao.getGameResultCountByUser(sessionUser, GameResult.WIN));
+		request.setAttribute("sessionUserLossCount", gameDao.getGameResultCountByUser(sessionUser, GameResult.LOSS));
+		request.setAttribute("sessionUserDrawCount", gameDao.getGameResultCountByUser(sessionUser, GameResult.DRAW));
+
+		request.setAttribute("sessionUserRockCount", gameDao.getUserChoiceCountByUser(sessionUser, GameChoice.ROCK));
+		request.setAttribute("sessionUserPaperCount", gameDao.getUserChoiceCountByUser(sessionUser, GameChoice.PAPER));
+		request.setAttribute("sessionUserScissorsCount", gameDao.getUserChoiceCountByUser(sessionUser, GameChoice.SCISSORS));
+
+		List<Game> lastGames = gameDao.findByUser(sessionUser, 10);
+
+		request.setAttribute("lastGames", lastGames);
 	}
 }
