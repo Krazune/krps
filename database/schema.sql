fa378a38e64e3bba0b7335d70f8fb8162bcb3b79
Krazune <krazune@gmail.com>
2022-01-14 18:03:44 +0000

    Add games table

--- database/schema.sql
+++ database/schema.sql
@@ -23,9 +23,78 @@
 */
 CREATE EXTENSION IF NOT EXISTS citext;
 
+CREATE OR REPLACE FUNCTION kprs_compute_outcome(choice1 CHARACTER(1), choice2 CHARACTER(1))
+RETURNS CHARACTER(1)
+LANGUAGE PLPGSQL
+IMMUTABLE
+AS
+$$
+BEGIN
+	IF choice1 ILIKE 'r' THEN
+		IF choice2 ILIKE 'r' THEN
+			RETURN 'd';
+		END IF;
+
+		IF choice2 ILIKE 'p' THEN
+			RETURN 'l';
+		END IF;
+
+		IF choice2 ILIKE 's' THEN
+			RETURN 'w';
+		END IF;
+
+		RAISE EXCEPTION 'invalid parameter: %', choice2;
+	END IF;
+
+	IF choice1 ILIKE 'p' THEN
+		IF choice2 ILIKE 'r' THEN
+			RETURN 'w';
+		END IF;
+
+		IF choice2 ILIKE 'p' THEN
+			RETURN 'd';
+		END IF;
+
+		IF choice2 ILIKE 's' THEN
+			RETURN 'l';
+		END IF;
+
+		RAISE EXCEPTION 'invalid parameter: %', choice2;
+	END IF;
+
+	IF choice1 ILIKE 's' THEN
+		IF choice2 ILIKE 'r' THEN
+			RETURN 'l';
+		END IF;
+
+		IF choice2 ILIKE 'p' THEN
+			RETURN 'w';
+		END IF;
+
+		IF choice2 ILIKE 's' THEN
+			RETURN 'd';
+		END IF;
+
+		RAISE EXCEPTION 'invalid parameter: %', choice2;
+	END IF;
+
+	RAISE EXCEPTION 'invalid parameter: %', choice1;
+END;
+$$;
+
 CREATE TABLE IF NOT EXISTS users(
 	id SERIAL PRIMARY KEY,
 	username CITEXT UNIQUE NOT NULL,
 	password_hash VARCHAR(256) NOT NULL,
 	creation_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS games(
+	id SERIAL PRIMARY KEY,
+	user_id INT,
+	user_choice CHARACTER(1) NOT NULL CHECK(user_choice IN('r', 'p', 's')),
+	computer_choice CHARACTER(1) NOT NULL CHECK(computer_choice IN('r', 'p', 's')),
+	outcome CHARACTER(1) GENERATED ALWAYS AS (kprs_compute_outcome(user_choice, computer_choice)) STORED,
+	creation_date TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id)
 );
