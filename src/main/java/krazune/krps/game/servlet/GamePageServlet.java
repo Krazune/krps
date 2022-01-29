d00beb744efac4ca2edf4b0ccba5eb1b0bb74f24
Krazune <krazune@gmail.com>
2022-01-29 21:52:01 +0000

    Add session check to navigation

--- src/main/java/krazune/krps/game/servlet/GamePageServlet.java
+++ src/main/java/krazune/krps/game/servlet/GamePageServlet.java
@@ -5,12 +5,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import krazune.krps.user.Authentication;
 
 public class GamePageServlet extends HttpServlet
 {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		setupNavigationLinks(request);
+
 		request.getRequestDispatcher("/WEB-INF/jsp/game.jsp").forward(request, response);
+	}
+
+	private void setupNavigationLinks(HttpServletRequest request)
+	{
+		boolean authenticated = Authentication.getSessionUser(request.getSession()) != null;
+
+		request.setAttribute("showHomeLink", false);
+		request.setAttribute("showLoginLink", !authenticated);
+		request.setAttribute("showRegistrationLink", !authenticated);
+		request.setAttribute("showStatisticsLink", true);
+		request.setAttribute("showInformationLink", true);
+		request.setAttribute("showSettingsLink", authenticated);
+		request.setAttribute("showLogoutLink", authenticated);
 	}
 }
