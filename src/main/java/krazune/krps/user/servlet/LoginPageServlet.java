4ded1c0a11f42216d5ba354e996b715a67aa1711
Krazune <krazune@gmail.com>
2022-01-29 23:02:23 +0000

    Add basic login

--- src/main/java/krazune/krps/user/servlet/LoginPageServlet.java
+++ src/main/java/krazune/krps/user/servlet/LoginPageServlet.java
@@ -1,20 +1,56 @@
 package krazune.krps.user.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import krazune.krps.dao.DaoFactory;
+import krazune.krps.hash.HashGenerator;
 import krazune.krps.user.Authentication;
+import krazune.krps.user.User;
+import krazune.krps.user.dao.UserDao;
 
 public class LoginPageServlet extends HttpServlet
 {
+	private UserDao userDao;
+	private HashGenerator hashGenerator;
+
+	@Override
+	public void init() throws ServletException
+	{
+		userDao = ((DaoFactory) getServletContext().getAttribute("mainDaoFactory")).createUserDao();
+		hashGenerator = (HashGenerator) getServletContext().getAttribute("mainHashGenerator");
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		setupNavigationLinks(request);
 
 		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		setupNavigationLinks(request);
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		// Validate input.
+
+		if (authenticateUser(request.getSession(), username, password) == null)
+		{
+			invalidLoginRefresh(request, response);
+
+			return;
+		}
+
+		response.sendRedirect(request.getContextPath() + "/");
 	}
 
 	private void setupNavigationLinks(HttpServletRequest request)
@@ -28,5 +64,23 @@ public class LoginPageServlet extends HttpServlet
 		request.setAttribute("showInformationLink", true);
 		request.setAttribute("showSettingsLink", authenticated);
 		request.setAttribute("showLogoutLink", authenticated);
+	}
+
+	private User authenticateUser(HttpSession session, String username, String password) throws ServletException
+	{
+		try
+		{
+			return Authentication.authenticateUser(session, userDao, hashGenerator, username, password);
+		}
+		catch (SQLException e)
+		{
+			throw new ServletException(e);
+		}
+	}
+
+	private void invalidLoginRefresh(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		request.setAttribute("previousUsername", request.getParameter("username"));
+		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
 	}
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -7,5 +7,14 @@
 	</head>
 	<body class="page">
 		<%@ include file="/WEB-INF/jsp/fragments/header.jsp" %>
+		<form method="post" action="/login">
+		<label for="username">Username</label>
+		<input id="username" type="text" name="username" value="${previousUsername}">
+
+		<label for="password">Password</label>
+		<input id="password" type="password" name="password">
+
+		<input type="submit" name="login-submit" value="Log in">
+		</form>
 	</body>
 </html>
