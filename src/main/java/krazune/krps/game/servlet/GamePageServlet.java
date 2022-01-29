d609035502951756efc25980a41e72f5b3796286
Krazune <krazune@gmail.com>
2022-01-29 18:23:43 +0000

    Add game page base

--- src/main/java/krazune/krps/game/servlet/GamePageServlet.java
+++ src/main/java/krazune/krps/game/servlet/GamePageServlet.java
@@ -1,18 +1,16 @@
-package krazune;
+package krazune.krps.game.servlet;
 
 import java.io.IOException;
-import java.time.Instant;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet
+public class GamePageServlet extends HttpServlet
 {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		request.setAttribute("helloWorldDate", Instant.now());
-		request.getRequestDispatcher("/WEB-INF/jsp/hello-world.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/jsp/game.jsp").forward(request, response);
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/fragments/header.jsp
@@ -0,0 +1,16 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<header class="header">
+	<h2 class="text header__author">Krazune's</h2>
+	<h1 class="text text--special header__title">Rock Paper Scissors</h1>
+	<nav class="navigation">
+		<ul class="list navigation__list">
+			<li><a class="text link navigation__link" href="#">Home</a></li>
+			<li><a class="text link navigation__link" href="#">Login</a></li>
+			<li><a class="text link navigation__link" href="#">Registration</a></li>
+			<li><a class="text link navigation__link" href="#">Statistics</a></li>
+			<li><a class="text link navigation__link" href="#">Information</a></li>
+		</ul>
+	</nav>
+</header>
